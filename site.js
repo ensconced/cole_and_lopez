3163e2549e2650b98abceba7f5cac7639329a942
barnettjoe <joebarnett876@gmail.com>
2018-11-20 12:26:16 +0000

    form scrolling

--- site.js
+++ site.js
@@ -1,4 +1,5 @@
 (function () {
+
   var stuck;
   var nav = document.querySelector('nav');
   var svgWrapper = nav.querySelector('#svg-wrapper');
@@ -6,55 +7,54 @@
   var main = document.getElementById('main');
   var footer = document.querySelector('footer');
   var limit;
-
   var wait;
   var stagedCallback;
-
+  var isMobile = checkIfMobile();
   var formTimeout;
 
   // Cross browser compatible, non-overriding window.onload function
   if (window.addEventListener) {
-    window.addEventListener('load', init, false);
+    window.addEventListener('load', function () {
+      init(true);
+    }, false);
   } else {
     window.attachEvent && window.attachEvent('onload', init);
   }
 
-  window.onresize = function () {
-   // don't let keyboard trigger resize on mobile browsers
-   if($(document.activeElement).prop('type') === 'text') {
-     throttled(init, 500)();
-   }
-  };
+  window.onresize = throttled(function () {
+    console.log('resize');
+    if (!isMobile) {
+      resizeHandler();
+    } else {
+      scrollTo();
+      // scroll to active element
+
+    }
+  }, 500);
 
   window.addEventListener("orientationchange", function() {
-      init();
+      resizeHandler();
   });
 
-  // Initialize the page
   function init() {
-    debugger;
     setUpForm();
     limit = scrollLimit(windowDimensions());
-    console.log(limit);
-    // Fade in the body
-    document.body.classList.add('js-has-loaded');
-    // Otherwise check if page is already scrolled
-    setScrolledClass();
     shrinkLogo();
-    // Show/Hide the navbar on scroll
     window.onscroll = function() {
-      setScrolledClass();
       shrinkLogo();
     };
+    if (isMobile) {
+      document.ontouchstart = function (ev) {
+        if (!['INPUT', 'TEXTAREA'].includes(ev.target.tagName)) {
+          nav.style.display = 'block';
+        }
+      };
+    }
   }
 
-  // Set the js-has-scrolled class on the body depending on scroll position
-  function setScrolledClass() {
-    if (isScrolled() == true) {
-      document.body.classList.add('js-has-scrolled');
-    } else {
-      document.body.classList.remove('js-has-scrolled');
-    }
+  function resizeHandler() {
+    limit = scrollLimit(windowDimensions());
+    shrinkLogo();
   }
 
   // Check if the user has scrolled
@@ -158,7 +158,7 @@
         }, 10000);
         button.style.display = 'none';
         spinner.style.display = 'block';
-        $.ajax({
+               $.ajax({
           type: 'POST',
           url: contactFormHost + 'send_email',
           data: jqueryForm.serialize(),
@@ -179,6 +179,31 @@
         });
       }
     });
+  }
+
+  function handleTextArea() {
+    var textarea = document.querySelector('#form textarea#message');
+    var recaptchaWrapperWrapper = document.getElementById('recaptcha-wrapper-wrapper');
+    var formSpinner = document.getElementById('form-spinner');
+    var formButton = document.getElementById('form-button');
+    var footer = document.getElementById('footer');
+    var initialDisplayStyles = {
+      'recaptcha-wrapper-wrapper': recaptchaWrapperWrapper.style.display,
+      'form-spinner': formSpinner.style.display,
+      'form-button': formButton.style.display,
+      'footer': footer.style.display,
+    };
+    textarea.onfocus = function () {
+      [recaptchaWrapperWrapper, formSpinner, formButton, footer].forEach(function (element) {
+        element.style.display = 'none';
+      });
+    };
+
+    textarea.onblur = function() {
+      ['recaptcha-wrapper-wrapper', 'form-spinner', 'form-button', 'footer'].forEach(function (id) {
+        document.getElementById(id).style.display = initialDisplayStyles[id];
+      });
+    };
   }
 
   function flashSuccess() {
@@ -222,5 +247,23 @@
         stagedCallback();
       }
     });
+  }
+
+  function checkIfMobile() {
+    var check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+  };
+
+  ['#email', '#subject', '#message'].forEach(function (input) {
+    $(input).on('focus', scrollTo);
+  });
+
+  function scrollTo() {
+    setTimeout(function () {
+      if (['INPUT', 'TEXTAREA'].includes(document.activeElement.tagName)) {
+        document.documentElement.scrollTop += document.activeElement.getBoundingClientRect().top - 45;
+      }
+    }, 500);
   }
 }());
