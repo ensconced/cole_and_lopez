f4a34bd31ce5762e84762183aa4bcc7d990aa66c
barnettjoe <joebarnett876@gmail.com>
2019-01-12 13:57:32 +0000

    scrolling productions

---
+++ copyrightUpdater.js
@@ -0,0 +1,11 @@
+$(function () {
+  var copyright = document.getElementById('yearRange');
+  var year = (new Date()).getFullYear();
+  var secondPart;
+  if (year === 2018) {
+    secondPart = "";
+  } else {
+    secondPart = " - " + year;
+  }
+  copyright.innerHTML = '2018' + secondPart;
+});
