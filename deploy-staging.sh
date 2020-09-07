70e2d352a47c8071892c0bd1db54a1c9c420ea2b
Joe Barnett <joebarnett876@gmail.com>
2020-09-07 20:56:20 +0100

    testing staging deploy script

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -8,9 +8,13 @@ if [ "$branch" = staging ]
 then
   if [ -z "$(git status --porcelain)" ]
   then
-    rm CNAME
-    git commit -am "remove CNAME"
-    git push staging staging
+    ls CNAME
+    if ls CNAME;
+    then
+      rm CNAME
+      git commit -am "remove CNAME"
+      git push cole_and_lopez_stagingng staging
+    fi
     echo your changes will soon appear at https://ensconced.github.io/cole_and_lopez_staging/
   else
     echo git tree must be clean
