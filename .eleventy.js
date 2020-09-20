344dea4585b85e5f193babca33ba54fefc984e59
Joe Barnett <joebarnett876@gmail.com>
2020-09-20 14:38:58 +0100

    switch to eleventy

---
+++ .eleventy.js
@@ -0,0 +1,5 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/styles");
+};
