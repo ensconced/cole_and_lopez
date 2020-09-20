2afdebf94966573b7b74c0eebe6b745b7182a6f2
Joe Barnett <joebarnett876@gmail.com>
2020-09-20 14:47:19 +0100

    move config

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/styles");
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+    },
+  };
 };
